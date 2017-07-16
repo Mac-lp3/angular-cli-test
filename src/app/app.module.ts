066314f829ead7b821bd026934391c36f1fe8e7f
Mac-lp3 <staticlp3@gmail.com>
2017-07-16 19:00:04 -0400

    added third form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FirstFormComponent } from './first-form/first-form.component';
 import { SecondFormComponent } from './second-form/second-form.component';
+import { ThirdFormComponent } from './third-form/third-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FirstFormComponent,
-    SecondFormComponent
+    SecondFormComponent,
+    ThirdFormComponent
   ],
   imports: [
     BrowserModule, FormsModule, CommonModule
