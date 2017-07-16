03d1e8004afcd21e2aa090ce8c9cb2bf73dac250
Mac-lp3 <staticlp3@gmail.com>
2017-07-16 18:05:12 -0400

    added second form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { FistFormComponent } from './fist-form/fist-form.component';
+import { FirstFormComponent } from './first-form/first-form.component';
+import { SecondFormComponent } from './second-form/second-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FistFormComponent
+    FirstFormComponent,
+    SecondFormComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
